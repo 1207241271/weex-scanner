a5e7b089f41234197ce4b15869779063bd0a659e
yangxu <1207241271@qq.com>
2017-12-14 20:45:47 +0800

    init

---
+++ ios/Sources/WeexScannerModule.h
@@ -0,0 +1,14 @@
+//
+//  WeexScannerModule.h
+//  WeexPluginTemp
+//
+//  Created by 齐山 on 17/3/14.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WeexSDK/WeexSDK.h>
+
+@interface WeexScannerModule : NSObject<WXModuleProtocol>
+@property(nonatomic,copy)WXModuleCallback callBack;
+@end
---
+++ ios/Sources/WeexScannerModule.m
@@ -0,0 +1,81 @@
+//
+//  WeexScannerModule.m
+//  WeexPluginTemp
+//
+//  Created by  on 17/3/14.
+//  Copyright © 2017年 . All rights reserved.
+//
+
+#import "WeexScannerModule.h"
+#import <WeexPluginLoader/WeexPluginLoader.h>
+#import "SGQRCodeScanningVC.h"
+#import <AVFoundation/AVFoundation.h>
+
+@implementation WeexScannerModule
+@synthesize weexInstance;
+
+WX_PlUGIN_EXPORT_MODULE(weexScanner, WeexScannerModule)
+WX_EXPORT_METHOD(@selector(scanQR:callBack:))
+- (void)scanQR:(NSString *)title callBack:(WXModuleCallback)callback
+{
+    
+    self.callBack=callback;
+    
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (status == AVAuthorizationStatusNotDetermined) {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        SGQRCodeScanningVC *vc = [[SGQRCodeScanningVC alloc] init];
+                        UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:vc ];
+                        nav.navigationBar.barStyle=UIBarStyleBlack;
+                        vc.callBack = self.callBack;
+                        vc.navigationItem.title =title.length?title:@"扫一扫";
+                        [[weexInstance.viewController navigationController] presentViewController:nav animated:YES completion:nil];
+                    });
+                    // 用户第一次同意了访问相机权限
+                    NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                    
+                } else {
+                    // 用户第一次拒绝了访问相机权限
+                    NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                    self.callBack(@{@"status":@"error",@"msg":@"用户第一次拒绝了访问相机权限"});
+                }
+            }];
+        } else if (status == AVAuthorizationStatusAuthorized) { // 用户允许当前应用访问相机
+            SGQRCodeScanningVC *vc = [[SGQRCodeScanningVC alloc] init];
+            UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:vc ];
+            nav.navigationBar.barStyle=UIBarStyleBlack;
+            
+            vc.callBack = self.callBack;
+            vc.navigationItem.title =title.length?title:@"扫一扫";
+            [[weexInstance.viewController navigationController] presentViewController:nav animated:YES completion:nil];
+        } else if (status == AVAuthorizationStatusDenied) { // 用户拒绝当前应用访问相机
+            UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                
+            }];
+            
+            [alertC addAction:alertA];
+            [[weexInstance.viewController navigationController] presentViewController:alertC animated:YES completion:nil];
+            self.callBack(@{@"status":@"error",@"msg":@"用户拒绝当前应用访问相机"});
+            
+        } else if (status == AVAuthorizationStatusRestricted) {
+            NSLog(@"因为系统原因, 无法访问相册");
+            self.callBack(@{@"status":@"error",@"msg":@"因为系统原因, 无法访问相册"});
+        }
+    } else {
+        UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        
+        [alertC addAction:alertA];
+        [[weexInstance.viewController navigationController] presentViewController:alertC animated:YES completion:nil];
+        self.callBack(@{@"status":@"error",@"msg":@"未检测到您的摄像头"});
+    }
+}
+
+@end
